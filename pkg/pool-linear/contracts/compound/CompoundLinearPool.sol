131eee96d40072dd5c7ded047c93ea6e9d853e85
Daniel Martins <martinsda@gmail.com>
2022-11-08 10:32:46 -0300

    Add scaling / fixed-point math

--- pkg/pool-linear/contracts/compound/CompoundLinearPool.sol
+++ pkg/pool-linear/contracts/compound/CompoundLinearPool.sol
@@ -15,14 +15,16 @@
 pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 
-import "@balancer-labs/v2-interfaces/contracts/pool-linear/IStaticAToken.sol";
+import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/ERC20.sol";
+import "@balancer-labs/v2-solidity-utils/contracts/math/Math.sol";
 
 import "./CToken.sol";
 import "../LinearPool.sol";
 
 contract CompoundLinearPool is LinearPool {
-    // ILendingPool private immutable _lendingPool;
-    CToken private immutable wrappedToken;
+    using Math for uint256;
+
+    uint256 private immutable _rateScaleFactor;
 
     struct ConstructorArgs {
         IVault vault;
@@ -53,9 +55,16 @@ contract CompoundLinearPool is LinearPool {
             args.owner
         )
     {
-        // _lendingPool = IStaticAToken(address(args.wrappedToken)).LENDING_POOL();
-        // _require(address(args.mainToken) == IStaticAToken(address(args.wrappedToken)).ASSET(), Errors.TOKENS_MISMATCH);
-        wrappedToken = CToken(address(args.wrappedToken));
+        // The cToken Exchange Rate is scaled by the difference in decimals between the cToken and the underlying asset.
+        // See https://docs.compound.finance/v2/ on "Interpreting Exchange Rates"
+
+        // _getWrappedTokenRate is scaled e18
+        uint256 wrappedTokenDecimals = ERC20(address(args.wrappedToken)).decimals();
+        uint256 mainTokenDecimals = ERC20(address(args.mainToken)).decimals();
+
+        // This is always positive because we only accept tokens with <= 18 decimals
+        uint256 digitsDifference = Math.add(18, mainTokenDecimals).sub(wrappedTokenDecimals);
+        _rateScaleFactor = 10**digitsDifference;
     }
 
     function _toAssetManagerArray(ConstructorArgs memory args) private pure returns (address[] memory) {
@@ -68,6 +77,9 @@ contract CompoundLinearPool is LinearPool {
     }
 
     function _getWrappedTokenRate() internal view override returns (uint256) {
+        // Maybe calling an ICToken interface is a better way
+        CToken wrappedToken = CToken(address(getWrappedToken()));
+
         // When a market is launched, the cToken exchange rate (how much ETH one cETH is worth) begins at 0.020000
         // and increases at a rate equal to the compounding market interest rate.
         // For example, after one year, the exchange rate might equal 0.021591
@@ -76,9 +88,13 @@ contract CompoundLinearPool is LinearPool {
         // Each cToken is convertible into an ever increasing quantity of the underlying asset, as interest accrues in the market.
         // The exchange rate between a cToken and the underlying asset is equal to:
         // exchangeRate = (getCash() + totalBorrows() - totalReserves()) / totalSupply()
+        // See https://docs.compound.finance/v2/ctokens/#exchange-rate
 
-        // Compound's implementation
-        uint256 rate = wrappedToken.exchangeRateStored();
+        // This implementation is pulled from the CToken contract internal function exchangeRateStoredInternal
+        // See https://github.com/compound-finance/compound-protocol/blob/master/contracts/CToken.sol#L293
+
+        uint256 unscaledRate = wrappedToken.exchangeRateStored();
+        uint256 rate = unscaledRate.mul(_rateScaleFactor).divDown(FixedPoint.ONE);
         return rate;
     }
 }
